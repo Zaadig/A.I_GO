4fe7ab67c2f67165b33ffb6da18d5a3a2639ce23
ELALAOUI Mohamed_Seddiq <Mohamed_Seddiq.Elalaoui@bordeaux-inp.fr>
2023-04-10 00:27:07 +0200

    Added strong alpha beta player

--- myPlayer.py
+++ myPlayer.py
@@ -6,8 +6,9 @@ Right now, this class contains the copy of the randomPlayer. But you have to cha
 '''
 
 import time
-import Goban 
+import Goban
 from random import choice
+from copy import deepcopy
 from playerInterface import *
 
 class myPlayer(PlayerInterface):
@@ -27,48 +28,73 @@ class myPlayer(PlayerInterface):
     def getPlayerMove(self):
         if self._board.is_game_over():
             print("Referee told me to play but the game is over!")
-            return "PASS" 
-            
+            return "PASS"
+
         # Set the maximum depth for the alpha-beta search
-        max_depth = 3
-        
+        max_depth = 10
+
         # Set the initial alpha and beta values
         alpha = float('-inf')
         beta = float('inf')
-        
+
+        # Set a time limit for the search
+        start_time = time.time()
+        time_limit = 5
+
         # Get the legal moves for the current player
         legal_moves = [self._board.flat_to_name(m) for m in self._board.legal_moves()]
-        
+
+
         # If there is only one legal move, play it
         if len(legal_moves) == 1:
             move = legal_moves[0]
             print("I am playing ", move)
             self._board.push(self._board.name_to_flat(move))
             return move
-        
-        # Otherwise, use alpha-beta to find the best move
+
+        # Otherwise, use alpha-beta with iterative deepening to find the best move
         best_move = None
-        best_score = float('-inf')
-        for move in legal_moves:
-            self._board.push(self._board.name_to_flat(move))
-            score = self.alpha_beta(self._board, max_depth, alpha, beta, False)
-            self._board.pop()
-            if score > best_score:
+        for depth in range(1, max_depth + 1):
+            current_best_move, current_best_score = self.alpha_beta(self._board, depth, alpha, beta, start_time, time_limit)
+            if current_best_score is not None:
+                best_move = current_best_move
+                alpha = current_best_score
+            if time.time() - start_time > time_limit:
+                break
+
+        self._board.push(self._board.name_to_flat(self._board.flat_to_name(best_move)))
+
+        print("I am playing ", best_move)
+        return self._board.flat_to_name(best_move)
+
+    def alpha_beta(self, board, depth, alpha, beta, start_time, time_limit):
+        best_move = None
+        best_score = None
+        for move in board.legal_moves():
+            if time.time() - start_time > time_limit:
+                break
+            board.push(move)
+            score = self.minimax(board, depth - 1, alpha, beta, False, start_time, time_limit)
+            board.pop()
+            if best_score is None or score > best_score:
                 best_move = move
                 best_score = score
-            alpha = max(alpha, best_score)
-        self._board.push(self._board.name_to_flat(best_move))
-        print("I am playing ", best_move)
-        return best_move
-
-    def alpha_beta(self, board, depth, alpha, beta, is_maximizing_player):
+                alpha = max(alpha, best_score)
+                if beta <= alpha:
+                    break
+        return best_move, best_score
+    
+    def minimax(self, board, depth, alpha, beta, is_maximizing_player, start_time, time_limit):
         if depth == 0 or board.is_game_over():
             return self.evaluate(board)
+
         if is_maximizing_player:
             max_score = float('-inf')
             for move in board.legal_moves():
+                if time.time() - start_time > time_limit:
+                    break
                 board.push(move)
-                score = self.alpha_beta(board, depth - 1, alpha, beta, False)
+                score = self.minimax(board, depth - 1, alpha, beta, False, start_time, time_limit)
                 board.pop()
                 max_score = max(max_score, score)
                 alpha = max(alpha, max_score)
@@ -78,8 +104,10 @@ class myPlayer(PlayerInterface):
         else:
             min_score = float('inf')
             for move in board.legal_moves():
+                if time.time() - start_time > time_limit:
+                    break
                 board.push(move)
-                score = self.alpha_beta(board, depth - 1, alpha, beta, True)
+                score = self.minimax(board, depth - 1, alpha, beta, True, start_time, time_limit)
                 board.pop()
                 min_score = min(min_score, score)
                 beta = min(beta, min_score)
@@ -87,13 +115,43 @@ class myPlayer(PlayerInterface):
                     break
             return min_score
 
-    def evaluate(self,board):
+    def liberties(self, board, color):
+        visited = set()
+        liberties = 0
+
+        for i in range(board._BOARDSIZE):
+            for j in range(board._BOARDSIZE):
+                coord = board.flatten((i, j))
+                if coord not in visited and board._board[coord] == color:
+                    group, group_liberties = self.get_group_and_liberties(board, coord)
+                    visited.update(group)
+                    liberties += group_liberties
+
+        return liberties
+
+    def get_group_and_liberties(self, board, coord):
+        group = set([coord])
+        liberties = set()
+        stack = [coord]
+        color = board._board[coord]
+
+        while stack:
+            current = stack.pop()
+            for neighbor in board._get_neighbors(current):
+                if board._board[neighbor] == color and neighbor not in group:
+                    group.add(neighbor)
+                    stack.append(neighbor)
+                elif board._board[neighbor] == Goban.Board._EMPTY:
+                    liberties.add(neighbor)
+
+        return group, len(liberties)
+
+    def evaluate(self, board):
         '''
         Returns a score representing how advantageous the current board configuration is for the player
         with the given color. A positive score indicates an advantage for the player, and a negative score
         indicates a disadvantage.
         '''
-        # Check if the game is over and return a large score if it is.
         if board.is_game_over():
             winner = board.result()
             if winner == self._mycolor:
@@ -103,10 +161,31 @@ class myPlayer(PlayerInterface):
             else:
                 return 0
 
-        # Calculate the difference in score between the two players.
-        scores = (self._board._nbBLACK , self._board._nbWHITE)
-        score_diff = scores[self._mycolor -1] - scores[Goban.Board.flip(self._mycolor) -1]
-        return score_diff
+        # Calculate the scores
+        scores = (self._board._nbBLACK, self._board._nbWHITE)
+
+        # Calculate the number of legal moves for both players
+        legal_moves_count = len(board.legal_moves())
+
+        # Create a temporary copy of the board to switch perspectives
+        temp_board = deepcopy(board)
+
+        # Switch to the opponent's perspective and calculate the number of legal moves
+        temp_board.play_move(-1)  # Pass move for the current player
+        opponent_legal_moves_count = len(temp_board.legal_moves())
+
+        # Calculate the number of liberties for both players
+        my_liberties = self.liberties(board, self._mycolor)
+        opponent_liberties = self.liberties(board, Goban.Board.flip(self._mycolor))
+
+        # Calculate the evaluation score
+        score_diff = scores[self._mycolor - 1] - scores[Goban.Board.flip(self._mycolor) - 1]
+        legal_moves_diff = legal_moves_count - opponent_legal_moves_count
+        liberties_diff = my_liberties - opponent_liberties
+        evaluation_score = score_diff + legal_moves_diff + liberties_diff
+
+        return evaluation_score
+
 
 
 
